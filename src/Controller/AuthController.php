e3cf435a6633d29a5222d05d7ad66d143449421f
jeth cherry <jethrosumbeiywet@gmail.com.com>
2025-03-13 16:42:52 +0300

    added authcontroller

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\citizen_service_module\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+* controller for Authentication
+*/
+
+class AuthController extends ControllerBase{
+
+public function register(){
+}
+
+}
